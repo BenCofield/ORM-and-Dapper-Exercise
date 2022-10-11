d3897dfaefbed8fcc63ec86ace1020e0371c5fa4
Ben Cofield <benrcofield@gmail.com>
2022-10-11 10:53:21 -0400

    Completed

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace ORM_Dapper
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.IO;
+
+namespace ORM_Dapper
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
+            string connString = config.GetConnectionString("DefaultConnection");
+            
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var departmentRepo = new DapperDepartmentRepository(conn);
+
+            departmentRepo.InsertDepartment("Misc");
+
+            var departments = departmentRepo.GetAllDepartments();
+
+            foreach (var department in departments)
+            {
+                Console.WriteLine($"{department.DepartmentID} {department.Name}");
+            }
         }
     }
 }
